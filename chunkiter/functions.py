ffc309fb32a867b6284fc8466d139b9bc97bac7a
M Hoegner <pbmaxi@hoegners.de>
2023-12-20 17:15:10 -0500

    implement caching of array tuple generators

--- chunkiter/functions.py
+++ chunkiter/functions.py
@@ -20,31 +20,68 @@ def multihash(*args, binary=False):
   else: return h.hexdigest()
 
 class IterableH5Chunks(object):
-  def __init__(self, filename, name, chunksize=None, reverse=False):
+  def __init__(self, filename, name=None, chunksize=None, reverse=False):
     self.filename = filename
-    self.name = name
-    self.identifier = multihash(filename, name, str(chunksize))
+    self.identifier = multihash(filename, str(type(name)), str(name), str(chunksize), str(reverse))
     self.chunksize = chunksize
     self.reverse = reverse
 
     datafile = tables.open_file(self.filename, "r")
-    array = datafile.root[self.name]
-    self.shape = array.shape
-    self.size = int(np.prod(self.shape))
-    self.chunksize = chunksize if chunksize is not None else array.chunkshape[0]
+
+    if name is None:
+      if "data" in datafile.root: name = "data"
+      else:
+        name = []
+        i = 0
+        while True:
+          name_ = "data{}".format(i)
+          if name_ in datafile.root: name.append(name_)
+          else: break
+          i += 1
+    self.name = name
+
+    if type(self.name) in [list, tuple]:
+      self.shape = []
+      self.size = []
+      self.chunksize = []
+      if chunksize is not None: raise NotImplementedError("chunksize!=None not supported when yielding tuples")
+      for name_ in self.name:
+        array = datafile.root[name_]
+        self.shape.append( array.shape )
+        self.size.append( int(np.prod(array.shape)) )
+        self.chunksize.append( array.chunkshape[0] )
+    else:
+      array = datafile.root[self.name]
+      self.shape = array.shape
+      self.size = int(np.prod(self.shape))
+      self.chunksize = chunksize if chunksize is not None else array.chunkshape[0]
+
     datafile.close()
 
   def __iter__(self):
     datafile = tables.open_file(self.filename, "r")
-    array = datafile.root[self.name]
-    size = self.chunksize if self.chunksize is not None else array.chunkshape[0]
 
-    if self.reverse:
-      for startindex in reversed(range(0, self.shape[0], self.chunksize)):
-        yield array[startindex:startindex+self.chunksize,...][::-1,...]
-    else:
-      for startindex in range(0, self.shape[0], self.chunksize):
-        yield array[startindex:startindex+self.chunksize,...]
+    if type(self.name) in [list, tuple]: # yielding tuples of arrays
+      arrays = [datafile.root[name] for name in self.name]
+
+      if self.reverse:
+        startindices_iterators = [reversed(range(0, shape[0], chunksize)) for shape, chunksize in zip(self.shape, self.chunksize)]
+        for startindices in zip(*startindices_iterators):
+          yield tuple(array[startindex:startindex+chunksize,...][::-1,...] for array, startindex, chunksize in zip(arrays, startindices, self.chunksize))
+      else:
+        startindices_iterators = [range(0, shape[0], chunksize) for shape, chunksize in zip(self.shape, self.chunksize)]
+        for startindices in zip(*startindices_iterators):
+          yield tuple(array[startindex:startindex+chunksize,...] for array, startindex, chunksize in zip(arrays, startindices, self.chunksize))
+      
+    else: # yielding single arrays
+      array = datafile.root[self.name]
+
+      if self.reverse:
+        for startindex in reversed(range(0, self.shape[0], self.chunksize)):
+          yield array[startindex:startindex+self.chunksize,...][::-1,...]
+      else:
+        for startindex in range(0, self.shape[0], self.chunksize):
+          yield array[startindex:startindex+self.chunksize,...]
 
     datafile.close()
 
@@ -68,7 +105,7 @@ def yielding_chunks_to_h5(iterator, filename, name=None, expectedchunks=128, ver
   filenames = filename if type(filename)==tuple else (filename,)
   names = name if type(name)==tuple else (name,)
   # TO DO: check if all cases are properly handled
-  #   v1,   | fn      | None  | fn.data0
+  #   v1,   | fn      | None  | fn.data
   #   v1,v2 | fn      | None  | fn.data0 fn.data1
   #   v1,v2 | fn1,fn2 | None  | fn1.data0 fn2.data0
   #   v1,v2 | fn1,fn2 | n1,n2 | fn1.name1 fn2.name2
@@ -79,12 +116,15 @@ def yielding_chunks_to_h5(iterator, filename, name=None, expectedchunks=128, ver
   unnamed_counter = collections.Counter()
   datafiles = []
   datasets = []
+  nottuple = False
 
   for chunk_i,data in enumerate(iterator):
     data_original = data
     if preprocessor is not None: data = preprocessor(data)
 
-    if not type(data)==tuple: data = (data,)
+    if not type(data)==tuple:
+      data = (data,)
+      nottuple = True
 
     if len(filenames)==1 and len(data)>1: filenames = filenames*len(data)
     if len(names)==1 and len(data)>1: names = names*len(data)
@@ -97,7 +137,7 @@ def yielding_chunks_to_h5(iterator, filename, name=None, expectedchunks=128, ver
         occupied = []
         for fn,n,v in zip(filenames, names, data):
           if n is None:
-            n = "data{}".format(unnamed_counter[fn])
+            n = "data" if nottuple else "data{}".format(unnamed_counter[fn])
             unnamed_counter[fn] += 1
 
           if (fn,n) in occupied: raise ValueError("conflict: tried to write twice to dataset {} in filename {}".format(n,fn))
@@ -109,6 +149,7 @@ def yielding_chunks_to_h5(iterator, filename, name=None, expectedchunks=128, ver
           atom = tables.Atom.from_dtype(v.dtype)
           shape = (0,)+v.shape[1:]
           datafile.create_earray(datafile.root, n, atom=atom, shape=shape, chunkshape=v.shape, expectedrows=expectedchunks*v.shape[0], filters=filters)
+          print(n, "created")
           datasets.append(datafile.root[n])
 
       for d,v in zip(datasets, data):
@@ -137,8 +178,6 @@ def array_from_h5(filename, name):
 
 default_cachedir = "cache"
 def cache(iterator, identifier, *input_identifiers, active=True, cachedir=None, verbose=True):
-  # TO DO: handle iterators of tuples
-
   if type(identifier)==tuple: identifier, version = identifier
   else: version = "0"
 
@@ -154,7 +193,7 @@ def cache(iterator, identifier, *input_identifiers, active=True, cachedir=None, 
   # handle active=False - just pass through the iterator, but with an identifier attached
   if not active:
     iterator = iter(iterator) # just in case iterator is a list
-    return IdentifierIterator(iterator, path, "data0")
+    return IdentifierIterator(iterator, path)
 
   # TO DO: clean up files with same identifier but different version
 
@@ -166,7 +205,7 @@ def cache(iterator, identifier, *input_identifiers, active=True, cachedir=None, 
 
   if have_data:
     if verbose: print("using {}.".format(path))
-    return IterableH5Chunks(path, "data0")
+    return IterableH5Chunks(path)
 
   # otherwise, first compute it
   if os.path.exists(path): os.remove(path)
@@ -194,7 +233,7 @@ def cache(iterator, identifier, *input_identifiers, active=True, cachedir=None, 
     print("*"*80)
     print()
 
-  return IterableH5Chunks(path, "data0")
+  return IterableH5Chunks(path)
 
 def rechunk(iterator, chunksize):
   current_chunk = None
